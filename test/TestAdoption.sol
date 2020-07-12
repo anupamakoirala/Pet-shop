f40107e68fa1d20dc23018171ad858202c8bf54d
anupamakoirala <anukoirala.96@hotmail.com>
2020-07-12 22:03:04 +0545

    Truffle version degraded to solve truffle test error

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -23,25 +23,25 @@ contract TestAdoption {
     }
 
     // Testing retrieval of a single pet's owner
-    // function testGetAdopterAddressByPetId() public {
-    //     address adopter = adoption.adopters(expectedpetid);
+    function testGetAdopterAddressByPetId() public {
+        address adopter = adoption.adopters(expectedpetid);
 
-    //     Assert.equal(
-    //         adopter,
-    //         expectedAdopter,
-    //         "Owner of the expected pet should be this contract"
-    //     );
-    // }
+        Assert.equal(
+            adopter,
+            expectedAdopter,
+            "Owner of the expected pet should be this contract"
+        );
+    }
 
     // Testing retrieval of all pet owners
-//     function testGetAdopterAddressByPetIdInArray() public {
-//         // Store adopters in memory rather than contract's storage
-//         address[16] memory adopters = adoption.getAdopters();
+    function testGetAdopterAddressByPetIdInArray() public {
+        // Store adopters in memory rather than contract's storage
+        address[16] memory adopters = adoption.getAdopters();
 
-//         Assert.equal(
-//             adopters[expectedpetid],
-//             expectedAdopter,
-//             "Owner of the expected pet should be this contract"
-//         );
-//     }
-// }
+        Assert.equal(
+            adopters[expectedpetid],
+            expectedAdopter,
+            "Owner of the expected pet should be this contract"
+        );
+    }
+}
